95a2dfc06d4aa48b3360faa8bea04511bb4d97eb
oriverk <se.sterroristalqaeda@gmail.com>
2020-04-19 00:58:28 +0900

    make draft with html

--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,103 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Index() {
   return (
-    <div className="container">
+    <React.Fragment>
       <Head>
-        <title>Create Next App</title>
+        <title>NextPortfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+      <header>
+        <div className="header-link">
+        <h1 className="title"><a href="#home">Kawano Yudai</a></h1>
+        {/* component/header */}
+        <p>B.Agr / JobSeeker</p>
+        <nav>
+          <a href="#about">About</a>
+          <a href="#history">History</a>
+          <a href="#works">Works</a>
+          <a href="#blog">Blog</a>
+        </nav>
+        {/* component/header */}
+        <div>リンク類</div>
+          <p><small lang="en">ⓒ 2020 Kawano Yudai</small></p>
         </div>
+      </header>
+      <main>
+        {/* component/main */}
+        <section id="home"></section>
+        <section id="about">
+          <h2>About</h2>
+          <p>
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+            About About About About About About About About About About About
+          </p>
+        </section>
+        <br />
+        <section id="history">
+          <h2>History</h2>
+          <p>
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+            History History History History History History History History History 
+          </p>
+        </section>
+        <section id="works">
+          <h2>Works</h2>
+          <p>
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+            Works Works Works Works Works Works Works Works Works Works Works Works 
+          </p>
+        </section>
+        {/* component/main */}
       </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .header-link {
+          position: absolute;
+          left: 0;
+          bottom: 0;
         }
-
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          border: 1px solid yellow;
+          position: absolute;
+          top: 0;
+          left: 20%;
         }
 
-        footer {
+        #home {
+          display: block;
+          height: 100vh;
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+          background-color: gray;
+          background-image: url("/top20200418.jpg");
+          background-size: cover;
         }
 
         a {
@@ -102,8 +106,9 @@ export default function Home() {
         }
 
         .title a {
-          color: #0070f3;
+          color: #FFF;
           text-decoration: none;
+          font-weight: lighter;
         }
 
         .title a:hover,
@@ -118,87 +123,52 @@ export default function Home() {
           font-size: 4rem;
         }
 
-        .title,
-        .description {
-          text-align: center;
+        img {
+          height: 100vh;
+          width: 100%;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        nav a {
+          display: block;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
       `}</style>
 
       <style jsx global>{`
         html,
         body {
+          color: #FFF;
+          background-color: #121212;
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        #__next {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        
+        header {
+          margin-top: 0;
+          padding-top: 50px;
+          border:1px solid #FFF;
+          height: 100vh;
+          width: 20%;
+          text-align: center;
+          position: fixed;
+          top: 0;
+          left: 0;
         }
 
         * {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </React.Fragment>
   )
 }
