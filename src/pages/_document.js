e9500eeeb5120bf552922f4f7c02c647c563cf0e
oriverk <se.sterroristalqaeda@gmail.com>
2020-04-27 19:07:01 +0900

    modify theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="theme-color" content={Mytheme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
