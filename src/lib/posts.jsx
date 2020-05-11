ed26a51dfd90e26709b5ecb1633d78ab57d0bde3
oriverk <se.sterroristalqaeda@gmail.com>
2020-05-11 10:09:37 +0900

    implement getStaticProps and apply [id].js

--- src/lib/posts.jsx
+++ src/lib/posts.jsx
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = path.join(process.cwd(), './src/pages/posts')
+const postsDirectory = path.join(process.cwd(), './src/docs')
 
 export function getSortedPostsData() {
   // get files name under /posts
@@ -30,4 +30,39 @@ export function getSortedPostsData() {
       return -1
     }
   })
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory)
+  
+  // return bellow array
+  // [
+  //   {
+  //   params: {
+  //       id:'ssg-ssr'
+  //     }
+  //   },
+  // ]
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
+
+export function getPostData(id) {
+  // fetch data to render post with id and return post data
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  // use gray-matter to analyze post meta data
+  const matterResult = matter(fileContents)
+
+  // bring data together with id
+  return {
+    id,
+    ...matterResult.data
+  }
+
 }
---
+++ src/pages/posts/[id].jsx
@@ -0,0 +1,31 @@
+import Layout from '../../components/Layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
