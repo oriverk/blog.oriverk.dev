0eccd444a40d32e71d6b2e8870ce89d086bc688c
oriverk <se.sterroristalqaeda@gmail.com>
2020-05-12 13:30:32 +0900

    wip

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -15,26 +15,44 @@ export async function getStaticProps() {
 
 export default function Index({allPostsData}) {
   return (
-    <Layout>
-      <Head>
-        <title>Blog Index</title>
-      </Head>
-      <article>
-        <h1>Blog</h1>
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            // <li className={utilStyles.listItem} key={id}>
-            <li key={id}>
-              <Link href="/posts/[id]" as={`posts/${id}`}>
-                <a><h2>{title}</h2></a>
-              </Link>
-              <br />
-              {/* <small className={utilStyles.lightText}> */}
-              <time dateTime={date}>{date}</time>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </Layout>
+    <>
+      <Layout>
+        <Head>
+          <title>Blog Index</title>
+        </Head>
+        <article className="content">
+          <h1>Blog Posts</h1>
+          <ul>
+            {allPostsData.map(({ id, date, title }) => (
+              // <li className={utilStyles.listItem} key={id}>
+              <li key={id}>
+                <Link href="/posts/[id]" as={`posts/${id}`}>
+                  <a><h2>{title}</h2></a>
+                </Link>
+                <br/>
+                {/* <small className={utilStyles.lightText}> */}
+                <time dateTime={date}>{date}</time>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </Layout>
+      <style jsx>{`
+        .content {
+          width: 100%;
+          max-width: 1000px;
+          margin: 0 auto 10px auto;
+          padding: 5%;
+          flex-grow: 1;
+        }
+        h1{
+          text-align: center;
+        }
+        a {
+          color: #FFF;
+          {/* border-bottom: 0.25rem solid #50CAF9; */}
+        }
+      `}</style>
+    </>
   )
 }
--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -24,18 +24,36 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ postData }) {
   return (
-    <PostLayout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          {/* <Date dateString={postData.date} /> */}
-          <time dateTime={postData.date}>{postData.date}</time>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
-      </article>
-    </PostLayout>
+    <>
+      <PostLayout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article className="content">
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            {/* <Date dateString={postData.date} /> */}
+            <time dateTime={postData.date}>posted on: {postData.date}</time>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
+        </article>
+      </PostLayout>
+      <style jsx>{`
+        .content {
+          width: 100%;
+          max-width: 1000px;
+          margin: 0 auto 10px auto;
+          padding: 5%;
+          flex-grow: 1;
+        }
+        h1{
+          text-align: center;
+        }
+        a {
+          color: #FFF;
+          {/* border-bottom: 0.25rem solid #50CAF9; */}
+        }
+      `}</style>
+    </>
   )
 }
