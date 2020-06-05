db88b6feb7296bd471f5de21f698b793c7fd01b2
oriverk <se.sterroristalqaeda@gmail.com>
2020-06-05 17:44:14 +0900

    implement next-offline for pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,36 @@
+// const withMDX = require('@next/mdx')({
+//   extension: /\.mdx?$/
+// })
+
+const withOffline = require('next-offline');
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: true,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
@@ -13,9 +46,9 @@ module.exports = {
   },
 }
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-})
+module.exports = withOffline(nextConfig);
+
+// module.exports = withMDX({
+//   pageExtensions: ['js', 'jsx', 'md', 'mdx']
+// })
+
