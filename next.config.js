9005e170af881f08dac8171dfff112130a946fb2
oriverk <se.sterroristalqaeda@gmail.com>
2020-06-08 13:35:27 +0900

    modify pwa and sw

--- next.config.js
+++ next.config.js
@@ -2,34 +2,6 @@
 //   extension: /\.mdx?$/
 // })
 
-const withOffline = require('next-offline');
-const nextConfig = {
-  target: 'serverless',
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: true,
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-};
 
 
 module.exports = {
@@ -47,9 +19,38 @@ module.exports = {
   },
 }
 
-module.exports = withOffline(nextConfig);
-
 // module.exports = withMDX({
-//   pageExtensions: ['js', 'jsx', 'md', 'mdx']
-// })
+  //   pageExtensions: ['js', 'jsx', 'md', 'mdx']
+  // })
+  
+const withOffline = require('next-offline')
 
+module.exports = withOffline({
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: '/service-worker.js',
+          destination: '/_next/static/service-worker.js',
+        },
+      ]
+    },
+  },
+})
