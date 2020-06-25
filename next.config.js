b6e021fe9bb3b76b8d93a184373e2de392eb9d2f
oriverk <oriveryk@gmail.com>
2020-06-25 07:46:49 +0000

    change to next-pwa from next-offline for PWA

--- next.config.js
+++ next.config.js
@@ -23,34 +23,10 @@ module.exports = {
   //   pageExtensions: ['js', 'jsx', 'md', 'mdx']
   // })
   
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa')
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: '/service-worker.js',
-          destination: '/_next/static/service-worker.js',
-        },
-      ]
-    },
-  },
+module.exports = withPWA({
+    pwa: {
+        dest: 'public'
+    }
 })
