6db731998eb8f4c21cd71260d819c13b27d6b6a0
oriverk <oriveryk@gmail.com>
2020-06-26 04:03:15 +0000

    yarn add next-compose-plugin and modify next.config.js

--- next.config.js
+++ next.config.js
@@ -2,31 +2,21 @@
 //   extension: /\.mdx?$/
 // })
 
-
-
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-    return config
-  },
-  webpackDevMiddleware: config => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config
-  },
-}
-
 // module.exports = withMDX({
   //   pageExtensions: ['js', 'jsx', 'md', 'mdx']
   // })
   
 const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    }
-})
+const nextConfig = {}
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {pwa: { dest: 'public' }}
+    ],
+  ],
+  nextConfig
+)
