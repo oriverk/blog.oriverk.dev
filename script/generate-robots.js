6d35e37c213924a8166a7a4f8eb89f5bd3928aa4
oriverk <oriveryk@gmail.com>
2020-06-30 10:28:58 +0000

    update nodejs latest and create robots-generator

---
+++ script/generate-robots.js
@@ -0,0 +1,22 @@
+import path from 'path'
+import fs from 'fs-extra'
+
+const userAgent = '*'
+const allow = '/'
+const sitemap = '/sitemap.xml'
+const baseUrl = 'https://oriverk.dev'
+
+
+  const robots = `
+  User - agent: ${userAgent}
+  Allow: ${allow}
+  Sitemap: ${baseUrl}${sitemap}
+  Host: ${baseUrl}
+  `
+
+  fs.writeFileSync(
+    path.join(process.cwd(), 'public/robots.txt'),
+    robots
+  )
+  console.log('created robots.txt')
+
