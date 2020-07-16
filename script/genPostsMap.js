896edb5700fe42c222f5b68d006804c63e54942b
oriverk <oriveryk@gmail.com>
2020-07-16 16:51:58 +0000

    update postMap generate script for algolia data

--- script/genPostsMap.js
+++ script/genPostsMap.js
@@ -2,6 +2,8 @@
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
+const removeMd = require('remove-markdown')
+const assert = require('assert').strict
 
 const postsDirectory = path.join(process.cwd(), 'src/docs')
 
@@ -17,16 +19,18 @@ const allPostsData = fileNames.map((fileName) => {
   const create = matterResult.data.create
   const update = matterResult.data.update || ''
   const tags = LowerCaseTags || ''
+  const content = removeMd(matterResult.content).replace(/\n/gim, '').replace(/\s{2,}/gim , '')
   return {
     id,
     title,
     create,
     update,
-    tags
+    tags,
+    content
   }
 })
 
-const sortedPostsData = allPostsData.sort((a, b) => {
+const sortedData = allPostsData.sort((a, b) => {
   if (a.create < b.create) {
     return 1
   } else {
@@ -34,15 +38,24 @@ const sortedPostsData = allPostsData.sort((a, b) => {
   }
 })
 
+const postsMap = JSON.parse(fs.readFileSync(
+  path.join(process.cwd(), 'gen/postsMap.json'), 'utf8'
+))
+
 try {
-  fs.writeFileSync(
-    path.join(process.cwd(), 'gen/postsMap.json'),
-    JSON.stringify(sortedPostsData, undefined, 2),
-    'utf-8'
-  )
-  console.log('success')
-}catch (err) {
-  console.error(err)
+  assert.deepStrictEqual(sortedData, postsMap,'these data are different')
+} catch (err) {
+  try {
+    fs.writeFileSync(
+      path.join(process.cwd(), 'gen/postsMap.json'),
+      JSON.stringify(sortedData, undefined, 2)
+      , 'utf-8'
+    )
+    console.log('suceed in saving data as new postsMap.json')
+  } catch (err) {
+    console.error(err)
+    console.log('failed to save data as new postsMap.json')
+  }
 }
 
 // postPages.json
@@ -53,3 +66,25 @@ try {
 //   update: '2020-06-05',
 //   tags: ['qiita', 'react', 'next.js', 'remark.js', 'vercel'],
 // },
+
+
+// const assertion = assert.deepStrictEqual(test1, test2,'hogehoge')
+// https://nodejs.org/api/assert.html#assert_assert_deepstrictequal_actual_expected_message
+// if actual and expected is same, return false
+// and if is different, return 
+// assert.js: 103
+// throw new AssertionError(obj);
+//   ^
+
+//   AssertionError[ERR_ASSERTION]: Expected values to be strictly deep - equal:
+// + actual - expected ...Lines skipped
+
+// [
+//   {
+// + content: 'from Qita: 宮崎県COVID-19対策サイトを見つけ、出身者として何かしたいと思ったから、した。出身の宮崎県用の対策サイトが出来、同時に感染者数が3人に増えていたこと
+
+// ...
+// generatedMessage: true,
+//   code: 'ERR_ASSERTION',
+//     actual: [
+//       {
