a0fe8e3d97e6188990d0bae65ff404bae75620b4
oriverk <oriveryk@gmail.com>
2020-09-03 04:06:51 +0000

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -12,50 +12,55 @@ const withPWA = require('next-pwa')
 const optimizedImages = require('next-optimized-images')
 const { resolve } = require('path')
 
+
 const nextConfig = {
   webpack: (config) => {
     // next-optimized-images
     config.resolve.alias['@public'] = resolve(__dirname, 'public')
     return config
+  },
+}
+
+const nextPwaConfig = {
+  pwa: {
+    disable: process.env.MODE_ENV === 'development',
+    dest: 'public'
   }
+}
+
+const nextOptimizedImagesConfig = {
+  inlineImageLimit: 8192,
+  imagesFolder: 'images',
+  imagesName: '[name]-[hash].[ext]',
+  handleImages: ['jpeg', 'png', 'webp'],
+  removeOriginalExtension: true,
+  optimizeImages: true,
+  optimizeImagesInDev: false,
+  mozjpeg: {
+    quality: 85,
+  },
+  optipng: {
+    optimizationLevel: 3,
+  },
+  responsive: {
+    adapter: require('responsive-loader/sharp'),
+    sizes: [640, 960, 1200, 1800],
+    disable: process.env.MODE_ENV === 'development'
+  },
+  webp: {
+    preset: 'default',
+    quality: 85,
+  },
 }
 
 module.exports = withPlugins(
   [
     [
-      withPWA,
-      {
-        pwa: {
-          disable: process.env.MODE_ENV === 'development',
-          dest: 'public'
-        }
-      }
+      withPWA, nextPwaConfig
     ],
-    [optimizedImages, {
-      // these are the default values so you don't have to provide them if they are good enough for your use-case.
-      // but you can overwrite them here with any valid value you want.
-      inlineImageLimit: 8192,
-      imagesFolder: 'images',
-      imagesName: '[name]-[hash].[ext]',
-      handleImages: ['jpeg', 'png', 'webp'],
-      removeOriginalExtension: true,
-      optimizeImages: true,
-      optimizeImagesInDev: false,
-      mozjpeg: {
-        quality: 85,
-      },
-      optipng: {
-        optimizationLevel: 3,
-      },
-      responsive: {
-        adapter: require('responsive-loader/sharp'),
-        sizes: [640, 960, 1200, 1800],
-      },
-      webp: {
-        preset: 'default',
-        quality: 85,
-      },
-    }],
+    [
+      optimizedImages, nextOptimizedImagesConfig
+    ],
   ],
   nextConfig
 )
