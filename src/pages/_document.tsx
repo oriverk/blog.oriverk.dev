202bf483dcc2a547f77e5a3068356a4956832782
oriverk <oriveryk@gmail.com>
2020-10-19 09:50:32 +0000

    stop using amp in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { useAmp } from 'next/amp'
 import { GA_TRACKING_ID } from '../lib/gtag'
 import blogConfig from '../../blog.config'
-import AmpAnalytics from '../components/amp/AmpAnalytics'
-
-type AmpWrapProps = {
-  ampOnly?: any
-  nonAmp?: any
-}
-
-function AmpWrap({ ampOnly, nonAmp }: AmpWrapProps) {
-  const isAmp = useAmp()
-  if (ampOnly) return isAmp && ampOnly
-  return !isAmp && nonAmp
-}
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -41,49 +28,16 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <AmpWrap
-            ampOnly={
-              <AmpAnalytics
-                type='googleanalytics'
-                script={{
-                  vars: {
-                    account: GA_TRACKING_ID,
-                    gtag_id: GA_TRACKING_ID,
-                    config: {
-                      [GA_TRACKING_ID]: { groups: 'default' },
-                    },
-                  },
-                  triggers: {
-                    trackPageview: {
-                      on: 'visible',
-                      request: 'pageview',
-                    },
-                  },
-                }}
-              />
-            }
-          />
-          {/* Non-AMP - Google Analytics */}
-          <AmpWrap
-            nonAmp={
-              <>
-                <script
-                  async
-                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                />
-                <script
-                  dangerouslySetInnerHTML={{
-                    __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-                      gtag('config', '${GA_TRACKING_ID}');
-                    `,
-                  }}
-                />
-              </>
-            }
-          />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `,
+            }}
+          />  
         </body>
       </Html>
     )
