690d17bd25d51faf3d8639ef4499f271b28e8c14
oriverk <oriveryk@gmail.com>
2020-10-21 21:13:47 +0000

    apply One-off global selectors of styled-jsx to styles

--- src/components/IconsWrapper.tsx
+++ src/components/IconsWrapper.tsx
@@ -24,11 +24,17 @@ const HatenaSvg: React.FC<ClassProps> = props => {
   return <svg className={props.class} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M20.47 0C22.42 0 24 1.58 24 3.53v16.94c0 1.95-1.58 3.53-3.53 3.53H3.53C1.58 24 0 22.42 0 20.47V3.53C0 1.58 1.58 0 3.53 0h16.94zm-3.705 14.47c-.78 0-1.41.63-1.41 1.41s.63 1.414 1.41 1.414 1.41-.645 1.41-1.425-.63-1.41-1.41-1.41zM8.61 17.247c1.2 0 2.056-.042 2.58-.12.526-.084.976-.222 1.32-.412.45-.232.78-.564 1.02-.99s.36-.915.36-1.48c0-.78-.21-1.403-.63-1.87-.42-.48-.99-.734-1.74-.794.66-.18 1.156-.45 1.456-.81.315-.344.465-.824.465-1.424 0-.48-.103-.885-.3-1.26-.21-.36-.493-.645-.883-.87-.345-.195-.735-.315-1.215-.405-.464-.074-1.29-.12-2.474-.12H5.654v10.486H8.61zm.736-4.185c.705 0 1.185.088 1.44.262.27.18.39.495.39.93 0 .405-.135.69-.42.855-.27.18-.765.254-1.44.254H8.31v-2.297h1.05zm8.656.706v-7.06h-2.46v7.06H18zM8.925 9.08c.71 0 1.185.08 1.432.24.245.16.367.435.367.83 0 .38-.13.646-.39.804-.265.154-.747.232-1.452.232h-.57V9.08h.615z' /></svg>
 }
 
-const HomeIconStyle = css`
+const commonStyle = css`
+.icons{
+  display: flex;
+  flex-direction: row;
+  z-index: 100;
+}
+
 .icon{
   position: relative;
-  display: inline-block;
   text-decoration: none;
+  border-radius: 50%;
   width: 2rem;
   height: 2rem;
   margin: .5rem;
@@ -41,14 +47,62 @@ const HomeIconStyle = css`
   height: 1.9rem;
 }
 
+.icon[aria-expanded='false']{
+  display: none;
+}
+
+:global(.react-icons),
+:global(.homeIconSvg){
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  fill: #424242;
+  width: 1.25rem;
+  height: 1.25rem;
+}
+
 @media( min-width: 960px ){
-  .icon{
+  .icon {
     width: 2.25rem;
     height: 2.25rem;
   }
-  .icon:active{
+  .icon:active {
     width: 2.15rem;
     height: 2.15rem;
+  }
+  :global(.react-icons),
+  :global(.homeIconSvg){
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+}
+`
+
+const homeStyle = css`
+.icon {
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+  margin: .5rem;
+  background-color: #EEE;
+  border-radius: 50%;
+}
+`
+
+const columnStyle = css`
+.icons {
+  position: fixed;
+  bottom: .5rem;
+  right: .5rem;
+}
+
+@media( min-width: 960px ){
+  .icons{
+    display: flex;
+    flex-direction: column;
+    left: 91%;
+    bottom: 3rem;
   }
 }
 `
@@ -86,53 +140,11 @@ export const HomeIcons: React.FC<Props> = ({ openSearch }) => {
           <IconContext.Provider value={{ className: 'react-icons' }}><FaTwitter /></IconContext.Provider>
         </a>
       </div>
-      <style jsx>{HomeIconStyle}</style>
+      <style jsx>{commonStyle}</style>
+      <style jsx>{homeStyle}</style>
     </>
   )
 }
-
-const BlogIconsStyle = css`
-  .icons{
-    display: flex;
-    flex-direction: row;
-    position: fixed;
-    bottom: .5rem;
-    right: .5rem;
-    z-index: 100;
-  }
-
-  .icon{
-    position: relative;
-    text-decoration: none;
-    border-radius: 50%;
-    width: 2rem;
-    height: 2rem;
-    margin: .5rem;
-    background-color: #EEE;
-    border-radius: 50%;
-  }
-
-  .icon:hover, .icon:active{
-    border: 2px solid #424242;
-  }
-  
-  .icon[aria-expanded='false']{
-    display: none;
-  }
-
-  @media( min-width: 960px ){
-    .icons{
-      display: flex;
-      flex-direction: column;
-      left: 91%;
-      bottom: 3rem;
-    }
-    .icon{
-      width: 2.25rem;
-      height: 2.25rem;
-    }
-  }
-`
 
 export const PostsIcons: React.FC<Props> = ({ openSearch }) => {
   return (
@@ -149,7 +161,8 @@ export const PostsIcons: React.FC<Props> = ({ openSearch }) => {
           </a>
         </Link>
       </div>
-      <style jsx>{BlogIconsStyle}</style>
+      <style jsx>{commonStyle}</style>
+      <style jsx>{columnStyle}</style>
     </>
   )
 }  
@@ -200,7 +213,8 @@ export const PostIcons: React.FC<PostProps> = ({ title, id, tags }) => {
           <IconContext.Provider value={{ className: 'react-icons' }}><MdMoreHoriz /></IconContext.Provider>
         </a>
       </div>
-      <style jsx>{BlogIconsStyle}</style>
+      <style jsx>{commonStyle}</style>
+      <style jsx>{columnStyle}</style>
     </>    
   )
 }  
@@ -220,7 +234,8 @@ export const TagsIcons: React.FC = () => {
           </a>
         </Link>
       </div>
-      <style jsx>{BlogIconsStyle}</style>
+      <style jsx>{commonStyle}</style>
+      <style jsx>{columnStyle}</style>
     </>    
   )
 }  
@@ -254,7 +269,8 @@ export const TagIcons: React.FC = ()  => {
           <IconContext.Provider value={{ className: 'react-icons' }}><MdMoreHoriz /></IconContext.Provider>
         </a>
       </div>
-      <style jsx>{BlogIconsStyle}</style>
+      <style jsx>{commonStyle}</style>
+      <style jsx>{columnStyle}</style>
     </>    
   )
 }  
