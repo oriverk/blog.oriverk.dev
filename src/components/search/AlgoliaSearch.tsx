c57316066b704d495932c29ac6aeefd524621a09
oriverk <oriveryk@gmail.com>
2020-11-24 09:56:24 +0000

    modify components/search/AlgoliaSearch.tsx

--- src/components/search/AlgoliaSearch.tsx
+++ src/components/search/AlgoliaSearch.tsx
@@ -1,22 +1,17 @@
-import React, { Component } from 'react'
 import { indexName, searchClient } from './SearchClients'
 import { Configure, InstantSearch } from 'react-instantsearch-dom'
 import { CustomSearchBox } from './Connector'
 import { SearchResults } from './SearchResults'
 
-export class AlgoliaSearch extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <InstantSearch
-          indexName={indexName}
-          searchClient={searchClient}
-        >
-          <Configure hitsPerPage={10} />
-          <CustomSearchBox />
-          <SearchResults />
-        </InstantSearch>
-      </React.Fragment>
-    )
-  }
+export const AlgoliaSearch: React.FC = () => {
+  return (
+    <InstantSearch
+      indexName={indexName}
+      searchClient={searchClient}
+    >
+      <Configure hitsPerPage={10} />
+      <CustomSearchBox />
+      <SearchResults />
+    </InstantSearch>
+  )
 }
