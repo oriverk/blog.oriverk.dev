70add96fcf4503afbf8729530f959c9488345756
oriverk <oriveryk@gmail.com>
2020-11-24 09:55:10 +0000

    modify CustomSearchBox

--- src/components/search/Connector.tsx
+++ src/components/search/Connector.tsx
@@ -1,4 +1,7 @@
+import { useRouter } from 'next/router'
 import { connectPoweredBy, connectSearchBox, connectStateResults } from 'react-instantsearch-dom'
+import { IconContext } from 'react-icons'
+import { MdSearch } from 'react-icons/md'
 
 // const Hits = (props) => {
 //   const { hits } = props
@@ -45,43 +48,95 @@ const PoweredBy: React.FC = () => {
 }
 export const CustomPoweredBy = connectPoweredBy(PoweredBy)
 
-const SearchBox = ({ currentRefinement, refine }) => (
-  <>
-    <div>
-      <form noValidate action='' role='search'>
-        <input
-          placeholder='Search post'
-          type='search'
-          value={currentRefinement}
-          onChange={event => refine(event.currentTarget.value)}
-          autoFocus
-        />
-      </form>
-    </div>
-    <style jsx>{`
+const SearchBox = ({ currentRefinement, refine }) => {
+  const router = useRouter()
+
+  function handleOnChange(e){
+    e.preventDefault();
+    refine(e.currentTarget.value)
+    const path = e.currentTarget.value ? `/search/?q=${e.currentTarget.value}` : '/search/';
+    router.push(path, undefined, {shallow: true})
+  }
+  return (
+    <>
+      <div>
+        <form noValidate action='' role='search' onSubmit={e => {
+          e.preventDefault();
+        }}>
+          <input
+            placeholder='Search posts in English...'
+            type='search'
+            value={currentRefinement}
+            onChange={e => { handleOnChange(e) }}
+            autoFocus
+            onSubmit={e => { e.preventDefault();}}
+          />
+          <button type='submit' disabled={!currentRefinement}>
+            <IconContext.Provider value={{ className: 'react-icons' }}><MdSearch /></IconContext.Provider>
+          </button>
+        </form>
+      </div>
+      <style jsx>{`
       div{
-        width: 100%;
         margin: 0 auto 1.5rem;
       }
-      input {
+
+      form {
+        display: inline-flex;
+        border-bottom: .15rem solid #EEE;
+        width: 100%;
+      }
+
+      input[type='search'] {
+        color: #EEE;
         font-size: 1rem;
         height: 3rem;
         width: 100%;
-        padding: .5rem;
+        padding: 0;
         border: none;
-        border-radius: .5rem;
+        border-radius: 0;
+        outline: none;
+        background: none;
       }
-      input:active, input:focus{
-        border-radius: .5rem;
+
+      button[type='submit'], button[type='reset'] {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        outline: none;
+        padding: 0;
+        appearance: none;
       }
+
+      :global(.react-icons){
+        fill: #EEE;
+        width: 2rem;
+        height: 2rem;
+      }
+
+      /* clears the ‘X’ from Internet Explorer */
+      input[type=search]::-ms-clear { display: none; width : 0; height: 0; }
+      input[type=search]::-ms-reveal { display: none; width : 0; height: 0; }
+      /* clears the ‘X’ from Chrome */
+      input[type="search"]::-webkit-search-decoration,
+      input[type="search"]::-webkit-search-cancel-button,
+      input[type="search"]::-webkit-search-results-button,
+      input[type="search"]::-webkit-search-results-decoration { display: none; }
     `}</style>
-  </>
-)
+    </>
+  )
+}
+
 export const CustomSearchBox = connectSearchBox(SearchBox)
 
-const StateResults = ({ searchResults }) => {
+const StateResults = ({ error, searchResults }) => {
   const hasResults = searchResults && searchResults.nbHits !== 0
-  // const nbHits = searchResults && searchResults.nbHits // the number of hits
+  const nbHits = searchResults && searchResults.nbHits
+  if (error) {
+    return (
+      <div>An error happened {error.message}</div>
+    )
+  }
   if (hasResults) {
     return (
       null
@@ -102,4 +157,5 @@ const StateResults = ({ searchResults }) => {
     )
   }
 }
+
 export const CustomStateResults = connectStateResults(StateResults)
