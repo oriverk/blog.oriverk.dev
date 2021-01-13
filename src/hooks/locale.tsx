ff76cedd10598d2875077e8fabede4789b5ae142
oriverk <oriveryk@gmail.com>
2021-01-13 15:34:27 +0000

    modify useLocale hooks

--- src/hooks/locale.tsx
+++ src/hooks/locale.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect, createContext, useContext } from 'react'
 import { useRouter } from 'next/router'
 
+// import i18n from '../../i18n.config'
+
 type Locales = 'en' | 'ja'
 type ContextProps = {
+  currentLocale: Locales,
+  nextLocale: Locales,
   locale: Locales,
   toggleLocale: (currentLocale: Locales) => void
 }
@@ -10,19 +14,31 @@ type ContextProps = {
 const LocaleContext = createContext<Partial<ContextProps>>({})
 
 const LocaleProvider: React.FC = ({ children }) => {
-  const [value, setLocale] = useState<Locales | null>(null)
-
   const router = useRouter()
-  const { pathname, locale, locales } = router
+  const { pathname, locale, locales, defaultLocale } = router
   
-  function toggleLocale(currentLocale: Locales) {
+  const currentLocale = locale as Locales
+
+  function getNextLocale(currentLocale: Locales) {
     const indexOfLocale = locales.indexOf(currentLocale)
-    const newLocale = locales[(indexOfLocale + 1) % locales.length] as Locales
-    setLocale(newLocale)
+    // when locales = [a, b, c], newLocale changes b -> c -> a -> b -> ...
+    return locales[(indexOfLocale + 1) % locales.length] as Locales
+  }
+
+  const nextLocale = getNextLocale(currentLocale)
+
+  const [value, setLocale] = useState<Locales>(null)
+
+  function toggleLocale(currentLocale: Locales) {
+    const nextLocale = getNextLocale(currentLocale)
+    console.log(`locale: ${locale}, currentLocale: ${currentLocale}, newLocale: ${nextLocale}`)
+    if (locale !== nextLocale) {
+      setLocale(nextLocale)
+    }
   }
 
   useEffect(() => {
-    if (localStorage.getItem('locale') === null) {
+    if (!localStorage || localStorage.getItem('locale') === null) {
       localStorage.setItem('locale', locale)
       return
     }
@@ -31,7 +47,7 @@ const LocaleProvider: React.FC = ({ children }) => {
   }, [value])
   
   return (
-    <LocaleContext.Provider value={{ locale, toggleLocale } as ContextProps}>
+    <LocaleContext.Provider value={{ locale, currentLocale, nextLocale, toggleLocale } as ContextProps}>
       {children}
     </LocaleContext.Provider>
   )
