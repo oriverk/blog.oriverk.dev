64087dfbfd584619df9beb13cdc285a6bd6e9201
oriverk <oriveryk@gmail.com>
2021-01-13 09:37:13 +0000

    apply i18n locale (en as default ad ja) to date-fns component

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -1,11 +1,24 @@
 import { parseISO, format } from 'date-fns'
+import ja from 'date-fns/locale/ja'
+import enUs from 'date-fns/locale/en-US'
 
 type Props = {
-  className?: string,
-  dateString: string
+  dateString: string,
+  locale?: string
 }
 
-export const Date: React.FC<Props> = ({ className, dateString }) => {
+export const Date: React.FC<Props> = ({ dateString, locale }) => {
   const date = parseISO(dateString)
-  return <time className={className} dateTime={dateString}>{format(date, 'LLL d, yyyy')}</time>
+  if (locale && locale === 'ja') {
+    return (
+      <time dateTime={dateString}>
+        {format(date, 'yyyy年LLLd日', { locale: ja })}
+      </time>
+    )
+  }
+  return (
+    <time dateTime={dateString}>
+      {format(date, 'LLL d, yyyy', { locale: enUs })}
+    </time>
+  )
 }
