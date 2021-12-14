7abb609e0d5888823d1a125c435a27353b91878d
oriverk <oriveryk@gmail.com>
2021-12-15 03:27:58 +0900

    add svg icon components

---
+++ src/components/icons/pen.tsx
@@ -0,0 +1,33 @@
+import { styled } from 'goober'
+import type { SvgIconProps } from '../../types'
+
+interface Props extends SvgIconProps {
+  className?: string
+}
+
+const Component: React.VFC<Props> = (props) => {
+  const { title, label, color: fill, ...restProps } = props
+
+  return (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill={fill} aria-label={label || ''} {...restProps}>
+      {title && <title>{title}</title>}
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 000-1.41l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+    </svg>
+  )
+}
+
+const StyledComponent = styled(Component)`
+  fill: ${(props) => props.color};
+  width: ${(props) => props.size / 4 + 'rem'};
+  height: ${(props) => props.size / 4 + 'rem'};
+`
+
+const ContainerComponent: React.VFC<Partial<SvgIconProps>> = (props) => {
+  const { color = '#000', size = 4, role = 'presentation', ...rest } = props
+  const modified = { color, size, role, ...rest }
+
+  return <StyledComponent {...modified} />
+}
+
+export const PenIcon = ContainerComponent
---
+++ src/components/named-icon.tsx
@@ -0,0 +1,72 @@
+import { styled } from 'goober'
+
+interface PassedProps {
+  variant: 'none' | 'outline'
+  borderRadius: number
+  name: string
+  color: string
+  fontSize: number
+  width: number
+  height: number
+}
+
+interface Props extends PassedProps {
+  className?: string
+}
+
+const Component: React.FC<Props> = (props) => {
+  const { className, children, name } = props
+  return (
+    <div className={className}>
+      <span className="icon">{children}</span>
+      {name && <span className="iconName">{name}</span>}
+    </div>
+  )
+}
+
+const StyledComponent = styled(Component)`
+  border: ${(props) => (props.variant === 'outline' ? `1px solid ${props.color}` : 'none')};
+  border-radius: ${(props) => props.borderRadius / 4 + 'rem'};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => props.width / 4 + 'rem'};
+  height: ${(props) => props.height / 4 + 'rem'};
+  user-select: none;
+
+  .iconName {
+    display: block;
+    font-size: ${(props) => props.fontSize / 4 + 'rem'};
+    color: ${(props) => props.color};
+  }
+`
+
+const ContainerComponent: React.FC<Partial<PassedProps>> = (props) => {
+  const {
+    variant = 'outline',
+    borderRadius = 1,
+    name = '',
+    color = '#b3b3b3',
+    fontSize = 4,
+    width = 16,
+    height = 16,
+    children,
+  } = props
+
+  const modified = {
+    variant,
+    borderRadius,
+    name,
+    color,
+    fontSize,
+    width,
+    height,
+    children,
+  }
+
+  return <StyledComponent {...modified} />
+}
+
+const NamedIcon = ContainerComponent
+export default NamedIcon
