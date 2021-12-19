218a54c2d310560b80223bffbc4252c8414ae3b2
oriverk <oriveryk@gmail.com>
2021-12-19 23:36:53 +0900

    add component to parse and display datestring

---
+++ src/components/date-formatter.tsx
@@ -0,0 +1,12 @@
+import { parseISO, format } from "date-fns";
+
+interface Props {
+  dateString: string;
+}
+
+export const DateFormatter: React.VFC<Props> = (props) => {
+  const { dateString } = props;
+  const date = parseISO(dateString);
+  
+  return <time dateTime={dateString}>{format(date, 'yyyy.MM.dd')}</time>
+}
