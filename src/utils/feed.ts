318373eaa73752b54da418aa970f15b361093635
oriverk <oriveryk@gmail.com>
2022-01-19 16:44:35 +0900

    modify feed generate funcs

--- src/utils/feed.ts
+++ src/utils/feed.ts
@@ -3,26 +3,31 @@ import { getPostsData } from 'utils/markdown/getContentData'
 const blogPath = process.env.NEXT_PUBLIC_BLOG_PATH || ''
 
 export async function generateFeedXml(): Promise<string> {
+  const blogTitle = blogPath.replace('https://', '')
+  const today = new Date().toUTCString()
+  const thisYear = new Date().getUTCFullYear()
+
   let xml = `<?xml version='1.0' encoding="utf-8"?>
     <rss version='2.0'>
   `
   xml += `
     <channel>
-      <title>${blogPath.replace('https://', '')}</title>
+      <title>${blogTitle}</title>
       <link>${blogPath}/feed.xml</link>
       <language>ja</language>
-      <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+      <lastBuildDate>${today}</lastBuildDate>
       <docs>https://validator.w3.org/feed/docs/rss2.html</docs>
-      <copyright>© ${new Date().getFullYear()} Kawano Yudai.</copyright>
+      <copyright>© ${thisYear} Kawano Yudai.</copyright>
       <category>blog</category>
     </channel>
   `
 
   const { posts } = await getPostsData()
 
-  posts.forEach(({ fileName, frontMatter, mdxSource }) => {
+  posts.forEach(({ fileName, frontMatter }) => {
     const datetime = frontMatter.update || frontMatter.create
     const utcString = new Date(datetime).toUTCString()
+    const description = frontMatter.tags.join(', ')
 
     xml += `
       <item>
@@ -30,7 +35,7 @@ export async function generateFeedXml(): Promise<string> {
         <link>${blogPath}/entry/${fileName}/</link>
         <guid>entry/${fileName}</guid>
         <pubDate>${utcString}</pubDate>
-        <description>${frontMatter.tags.join(', ')}</description>
+        <description>${description}</description>
       </item>
     `
   })
