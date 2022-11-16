703d2b739bed286546febfe9002c7042c57f36ab
oriverk <oriveryk@gmail.com>
2022-11-16 15:46:12 +0900

    mv markdown related libraries from dependency to devDependency

--- src/utils/markdown/serializeMdx.ts
+++ src/utils/markdown/serializeMdx.ts
@@ -1,12 +1,13 @@
 import type { SerializeOptions } from 'next-mdx-remote/dist/types'
 import { serialize } from 'next-mdx-remote/serialize'
+import remarkGfm from 'remark-gfm'
 import remarkComment from 'remark-comment'
 import rehypeSlug from 'rehype-slug'
 
 export async function serializeMdx(source: string) {
   const serializeOptions: SerializeOptions = {
     mdxOptions: {
-      remarkPlugins: [remarkComment],
+      remarkPlugins: [remarkComment, remarkGfm],
       rehypePlugins: [rehypeSlug],
     },
     parseFrontmatter: true,
